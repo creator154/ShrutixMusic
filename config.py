4119122dedf5e5c4256110b953cd814fa7794734
creator154 <cteator898@gmail.com>
2026-06-03 09:38:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,12 +49,12 @@ API_KEY = getenv(
 
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL",
-    "https://t.me/ShrutiBots"
+    "https://t.me/Updates_Jiosaavn"
 )
 
 SUPPORT_CHAT = getenv(
     "SUPPORT_CHAT",
-    "https://t.me/ShrutiSupportChat"
+    "https://t.me/+FvMi1igOXv1iMDI1"
 )
 
 AUTO_LEAVING_ASSISTANT = getenv(
